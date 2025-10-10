f97f038976dfb2cf32cd309d4a6d806a0f7a3b65
João Victor Santos <joaovictorsantos.20122009@gmail.com>
2025-10-10 04:45:27 +0000

    https://checkout.instabostt.store/VCCL1O8SCCNA LINK PARA O PACOTE 1 (_fo

--- src/data/packages-data.ts
+++ src/data/packages-data.ts
@@ -11,7 +11,7 @@ export type Package = {
 };
   
 export const followerPackagesData: Omit<Package, 'originalPrice' | 'isDiscount'>[] = [
-    { followers: 2250, price: 9.90, features: ["Seguidores 100% Brasileiros", "Não afeta o desempenho", "Garantia anti-queda"], checkoutLink: "https://checkout.instabostt.store/VCCL1O8SCCNM" },
+    { followers: 2250, price: 9.90, features: ["Seguidores 100% Brasileiros", "Não afeta o desempenho", "Garantia anti-queda"], checkoutLink: "https://checkout.instabostt.store/VCCL1O8SCCNA" },
     { followers: 5000, price: 19.90, features: ["Seguidores 100% Brasileiros", "Não afeta o desempenho", "Garantia anti-queda"], checkoutLink: "https://checkout.instabostt.store/VCCL1O8SCCNL" },
     { followers: 11250, price: 34.90, isPopular: true, features: ["Seguidores 100% Brasileiros", "Não afeta o desempenho", "Garantia anti-queda"], checkoutLink: "https://checkout.instabostt.store/VCCL1O8SCCNJ" },
     { followers: 22500, price: 59.90, isBestValue: true, features: ["Seguidores 100% Brasileiros", "Não afeta o desempenho", "Garantia anti-queda"], checkoutLink: "https://checkout.instabostt.store/VCCL1O8SCCNI" },
