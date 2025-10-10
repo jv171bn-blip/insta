945d1e488ecc37a754fe89aa071854efa8f38968
João Victor Santos <joaovictorsantos.20122009@gmail.com>
2025-10-10 15:40:34 +0000

    Remova esse pacote (_for element src/app/page.tsx > <CarouselItem>_)

--- src/components/sections/combo-packages.tsx
+++ src/components/sections/combo-packages.tsx
@@ -11,7 +11,6 @@ import {
 import { followerPackagesData, type Package } from "@/data/packages-data";
 
 const comboPackagesData: Omit<Package, 'price' | 'originalPrice' | 'isDiscount'>[] = [
-    { followers: 2250, likes: 750, features: ["Seguidores e Curtidas 100% Brasileiros", "Não afeta o desempenho", "Garantia anti-queda"], checkoutLink: "https://checkout.instabostt.store/VCCL1O8SCCO4" },
     { followers: 5000, likes: 1650, features: ["Seguidores e Curtidas 100% Brasileiros", "Não afeta o desempenho", "Garantia anti-queda"], checkoutLink: "https://checkout.instabostt.store/VCCL1O8SCCO5" },
     { followers: 11250, likes: 3750, features: ["Seguidores e Curtidas 100% Brasileiros", "Não afeta o desempenho", "Garantia anti-queda"], isPopular: true, checkoutLink: "https://checkout.instabostt.store/VCCL1O8SCCO6" },
     { followers: 22500, likes: 7500, features: ["Seguidores e Curtidas 100% Brasileiros", "Não afeta o desempenho", "Garantia anti-queda"], isBestValue: true, checkoutLink: "https://checkout.instabostt.store/VCCL1O8SCCO7" },
