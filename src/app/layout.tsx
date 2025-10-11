d4d35afe976edd47b88ef09fb38a39e78a3dd948
João Victor Santos <joaovictorsantos.20122009@gmail.com>
2025-10-11 00:50:55 +0000

    1246894817453773 É MEU ID

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '984920233660852');
+            fbq('init', '1246894817453773');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -50,7 +50,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=984920233660852&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1246894817453773&ev=PageView&noscript=1"
           />
         </noscript>
         {/* End Meta Pixel Code */}
