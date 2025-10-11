f8bdff67ea7ea7b3b92bc43a0c9257400081b2e6
João Victor Santos <joaovictorsantos.20122009@gmail.com>
2025-10-11 00:50:24 +0000

    esse dai é meu id

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "InstaBoost - Aumente sua Presença no Instagram",
-  description: "Compre seguidores para o Instagram e aumente sua presença instantaneamente.",
+  description:
+    "Compre seguidores para o Instagram e aumente sua presença instantaneamente.",
 };
 
 export default function RootLayout({
@@ -16,17 +18,46 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className="!scroll-smooth">
       <head>
+        {/* Google Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
+
+        {/* Meta Pixel Code */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '984920233660852');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=984920233660852&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
       </head>
+
       <body
-        className={cn(
-          "min-h-screen bg-background font-body antialiased"
-        )}
+        className={cn("min-h-screen bg-background font-body antialiased")}
       >
         {children}
         <Toaster />
