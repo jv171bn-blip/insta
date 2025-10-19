c1c23ceb6208d2bd526f87a466c516e42217dfd7
João Victor Santos <joaovictorsantos.20122009@gmail.com>
2025-10-19 06:52:15 +0000

    Desculpe, eu apenas nao tive paciencia pra esperar, esta funcionando tud

--- src/app/whats/route.ts
+++ src/app/whats/route.ts
@@ -1,17 +1,14 @@
 import { NextResponse } from 'next/server';
 
-// Garante que a rota seja dinâmica e não seja cacheada
-export const dynamic = 'force-dynamic';
-
 const links = [
   "https://wa.me/556281563542?text=Ol%C3%A1%2C+Tenho+interesse+em+turbinar+meu+Perfil+do+instagram%21",
   "https://wa.me/5511968264136?text=Ol%C3%A1%2C+Tenho+interesse+em+turbinar+meu+Perfil+do+instagram%21"
 ];
 
-export async function GET() {
-  const randomIndex = Math.floor(Math.random() * links.length);
-  const chosenLink = links[randomIndex];
+const randomIndex = Math.floor(Math.random() * links.length);
+const chosenLink = links[randomIndex];
 
+export async function GET() {
   console.log(`👉 Redirecionando para: ${chosenLink}`);
   
   return NextResponse.redirect(chosenLink, 302);
