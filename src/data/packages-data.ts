268a47d1a97269fb109732f4cd601ced8e6baa97
João Victor Santos <joaovictorsantos.20122009@gmail.com>
2025-12-10 03:45:14 +0000

    troque pra esse https://checkout.instasheik.shop/VCCL1O8SCCNO (_for elem

--- src/data/packages-data.ts
+++ src/data/packages-data.ts
@@ -16,6 +16,6 @@ export const followerPackagesData: Omit<Package, 'originalPrice' | 'isDiscount'>
     { followers: 22500, price: 59.90, features: ["Seguidores 100% Brasileiros", "Não afeta o desempenho", "Garantia anti-queda"], checkoutLink: "https://checkout.instasheik.shop/VCCL1O8SCCNL" },
     { followers: 45000, price: 99.90, features: ["Seguidores 100% Brasileiros", "Não afeta o desempenho", "Garantia anti-queda"], checkoutLink: "https://checkout.instasheik.shop/VCCL1O8SCCNM" },
     { followers: 67500, price: 129.90, features: ["Seguidores 100% Brasileiros", "Não afeta o desempenho", "Garantia anti-queda"], checkoutLink: "https://checkout.instasheik.shop/VCCL1O8SCCNN" },
-    { followers: 112500, price: 199.90, features: ["Seguidores 100% Brasileiros", "Não afeta o desempenho", "Garantia anti-queda"], checkoutLink: "https://checkout.instasheik.store/VCCL1O8SCCNO" },
+    { followers: 112500, price: 199.90, features: ["Seguidores 100% Brasileiros", "Não afeta o desempenho", "Garantia anti-queda"], checkoutLink: "https://checkout.instasheik.shop/VCCL1O8SCCNO" },
 ];
   
